8e4f94d788f25f0dc0348b23abe213fbd3064596
MmagdyS <muhammad.magdy50@gmail.com>
2019-05-10 11:03:12 +0200

    [+] Stop and wait (lost acks/ code performace)

--- RDT.py
+++ RDT.py
@@ -19,14 +19,15 @@ class RequestHandler(object):
 		self.data_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 		self.data_socket.bind((self.server_addr, 0))
-		# self.data_socket.settimeout(0.5)
+		self.data_socket.settimeout(0.5)
 
 		print(request)
 
 		self.seq_no, self.file_name = packet.packet_parser(request)
+		self.seq_no = random.randrange(65536)
 		self.data_socket.sendto(packet.create_udp_ack(self.seq_no), self.dest_addr)
 		self.file_name = self.file_name.decode("utf-8")
-		print(self.file_name)
+		# print(self.file_name)
 
 		if method == "stop_and_wait":
 			self.stop_wait()
@@ -36,27 +37,35 @@ class RequestHandler(object):
 
 
 
+	def send(self, data):
+		
+		data_packet = packet.create_udp_packet(self.seq_no, data)
+		print(data_packet)
+		while 1:
+			try:
+				self.data_socket.sendto(data_packet, self.dest_addr)
+				ack = self.data_socket.recvfrom(1024)
+				print("ACKED")
+				break
+			except socket.timeout:
+				continue		
+		
+
+
 	def stop_wait(self):
 		
-		self.seq_no = random.randrange(65536)
+		# self.seq_no = random.randrange(65536)
 
 		with open("tests/" + self.file_name, "rb") as f :
 
 			data = f.read(self.max_data_size)
 
 			while data:
-				data_packet = packet.create_udp_packet(self.seq_no, data)
-				print(data_packet)
-				self.data_socket.sendto(data_packet, self.dest_addr)
-				ack = self.data_socket.recvfrom(1024)
-				print("ACK:: ") 
-				print(ack)
+				self.send(data)
 				data = f.read(self.max_data_size)
 				self.seq_no = ( self.seq_no + 1 ) % 65538
 
-		data_packet = packet.create_udp_packet(self.seq_no, "EOF")
-		self.data_socket.sendto(data_packet, self.dest_addr)
-		ack = self.data_socket.recvfrom(1024)
+		self.send("EOF")
 
 		self.data_socket.close()
 
--- client.py
+++ client.py
@@ -6,7 +6,7 @@ def request_file(file_name, server_port, server_ip="127.0.0.1"):
 
 	client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	client_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	client_socket.settimeout(0.5)
+	client_socket.settimeout(1)
 	client_socket.bind(("", 0))
 
 	request = packet.create_udp_packet(0, file_name)
@@ -16,6 +16,9 @@ def request_file(file_name, server_port, server_ip="127.0.0.1"):
 		try:
 			client_socket.sendto(request, (server_ip, server_port))
 			data_packet, addr = client_socket.recvfrom(1024)
+			exp_seq, data = packet.packet_parser(data_packet)
+			print(data_packet)
+			print("move on")
 			break
 
 		except socket.timeout:
@@ -24,19 +27,28 @@ def request_file(file_name, server_port, server_ip="127.0.0.1"):
 	while 1:
 
 		data_packet, addr = client_socket.recvfrom(1024)
+		# print(data_packet)
 		seq_no, data = packet.packet_parser(data_packet)
-		print(data)
+		# print(data)
 
-		if data == b'EOF':
+		if seq_no == exp_seq:
+
+			if data == b'EOF':
+				ack = packet.create_udp_ack(seq_no)
+				break
+			print(data)
+			with open(file_name, "ab") as f:
+				f.write(data)
+
 			ack = packet.create_udp_ack(seq_no)
-			break
+			client_socket.sendto(ack, addr)
+			print(ack)
+			exp_seq += 1
 
-		with open(file_name, "ab") as f:
-			f.write(data)
+		elif seq_no < exp_seq:
+			ack = packet.create_udp_ack(seq_no)
+			client_socket.sendto(ack, addr)
 
-		ack = packet.create_udp_ack(seq_no)
-		client_socket.sendto(ack, addr)
-		print(ack)
 
 
 	client_socket.close()
